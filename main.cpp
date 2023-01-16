01a39bc3301654fbb787fc6bafc4953c8151043c
alp2008 <118907608+alp2008@users.noreply.github.com>
2023-01-16 18:00:42 +0400

    да

--- main.cpp
+++ main.cpp
@@ -11,7 +11,43 @@ int main()
 txCreateWindow (800, 600)
 txBitBlt (txDC(), xFon, yFon,3600,2400,Fon);
 
-txTransparentBlt (txDC(), xSpaceman2, ySpaceman2,147,150,Spaceman2,TX_BLACK);
+if(GetAsyncKeyState('w'))
+{
+    yFon -= 10;
+}
+if(GetAsyncKeyState('s'))
+{
+    yFon += 10;
+}
+if(GetAsyncKeyState('a'))
+{
+    xFon -= 10;
+}
+if(GetAsyncKeyState('d'))
+{
+    xFon += 10;
+}
+
+txTransparentBlt (txDC(), xSpaceman, ySpaceman,,,Spaceman, 0,0 TX_BLACK);
+if(GetAsyncKeyState(VK_UP))
+{
+    ySpaceman -= 10;
+}
+if(GetAsyncKeyState(VK_DOWN))
+{
+    ySpaceman += 10;
+}
+if(GetAsyncKeyState(VK_RIGHT))
+{
+    ySpaceman += 10;
+}
+if(GetAsyncKeyState(VK_LEFT))
+{
+    ySpaceman -= 10;
+}
+
+
+txTransparentBlt (txDC(), xSpaceman2, ySpaceman2,,,Spaceman2, 0,0 TX_BLACK);
 xSpaceman2 = xSpaceman2 +vxSpaceman2;
 
     if(xSpaceman2 > 800 - 147 || xSpaceman2 < 0)
