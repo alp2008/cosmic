b8d9bdfd5ad42565163e576ecb6685a9abc30db2
alp2008 <118907608+alp2008@users.noreply.github.com>
2023-01-19 21:40:38 +0400

    2

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include "TXLib.h"
-
+    int xSpaceman = 200;
+    int ySpaceman = 196;
     int xSpaceman2 = 100;
     int ySpaceman2 = 100;
     int vxSpaceman = 10;
@@ -7,6 +8,10 @@
     int yFon = -1200;
 
 int main()
+{
+HDC  Fon = txLoadImage ("fon.bmp");
+HDC  Spaceman = txLoadImage ("kosm.bmp");
+HDC  Spaceman2 = txLoadImage ("kosm2.bmp");
     {
 txCreateWindow (800, 600)
 txBitBlt (txDC(), xFon, yFon,3600,2400,Fon);
@@ -28,7 +33,7 @@ if(GetAsyncKeyState('d'))
     xFon += 10;
 }
 
-txTransparentBlt (txDC(), xSpaceman, ySpaceman,,,Spaceman, 0,0 TX_BLACK);
+txTransparentBlt (txDC(), xSpaceman, ySpaceman,200,196,Spaceman,TX_BLACK);
 if(GetAsyncKeyState(VK_UP))
 {
     ySpaceman -= 10;
@@ -47,7 +52,7 @@ if(GetAsyncKeyState(VK_LEFT))
 }
 
 
-txTransparentBlt (txDC(), xSpaceman2, ySpaceman2,,,Spaceman2, 0,0 TX_BLACK);
+txTransparentBlt (txDC(), xSpaceman2, ySpaceman2,200,196,Spaceman2, 0,0, TX_BLACK);
 xSpaceman2 = xSpaceman2 +vxSpaceman2;
 
     if(xSpaceman2 > 800 - 147 || xSpaceman2 < 0)
@@ -57,7 +62,19 @@ xSpaceman2 = xSpaceman2 +vxSpaceman2;
 
 
     txEnd;
+    {
+    {
+    while(  GetAsyncKeyState (VK_ESCAPE))
+        txSetFillColor(TX_BLACK);
+        txSetColor(TX_WHITE);
+        txClear();
+
+        txBitBlt(txDC(),0,0,800,600,Fon);
+        txTransparentBlt (txDC(), xspaceman,yspaceman, 200,196, spaceman 0,0, TX_BLACK)
+    }
+    txDeleteDC (Fon);
 
     txTextCursor (false);
     return 0;
     }
+
